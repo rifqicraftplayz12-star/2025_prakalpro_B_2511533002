bdf3781d034ad9daf8fb15babb9c37e6fc88f4b0
Aditia <Aditia@LAPTOP-IVQG7DL3>
2025-09-21 20:51:12 +0700

    tugasAlproPekan2

---
+++ prakalpro_2025_B_2511533002/src/Pekan2/tugasAlproPekan2.java
@@ -0,0 +1,19 @@
+package Pekan2;
+
+public class tugasAlproPekan2 {
+
+	public static void main(String[] args) {
+		int suhu = 29;
+		float kelembapan = 85.5f;
+		char kodecuaca = 'H';
+		boolean sedanghujan = false;
+				
+		System.out.println("======================LAPORAN CUACA HARI INI=====================");
+		System.out.println("Suhu Udara    : " + suhu + "°C");
+		System.out.println("Kelembapan    : " + kelembapan + "%");
+		System.out.println("Kode Kondisi  : " + kodecuaca);
+		System.out.println("Status Cuaca  : " + sedanghujan);
+		
+	}
+
+}
