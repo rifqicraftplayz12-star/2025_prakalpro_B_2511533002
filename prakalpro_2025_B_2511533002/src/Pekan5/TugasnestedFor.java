8267ead44583e5bae3b65578ae55b3c4913acdee
RifqiAditya599 <rifqicraft.playz12@gmail.com>
2025-11-02 08:02:16 +0700

    tugas

--- prakalpro_2025_B_2511533002/src/Pekan5/TugasnestedFor.java
+++ prakalpro_2025_B_2511533002/src/Pekan5/TugasnestedFor.java
@@ -3,11 +3,47 @@ package Pekan5;
 public class TugasnestedFor {
 
 	public static void main(String[] args) {
-		int n=5;
-		for (int i=1; i<=n; i++) {
-			for (int j=1; j<=n; j++) {
-			}
-		}
-	}
 
-}//
+        for (int i = 0; i < 10; i++) {
+            
+            if (i == 0 || i == 9) {
+                System.out.print("#");
+                
+                for (int j = 0; j < 16; j++) {
+                    System.out.print("=");
+                }
+                
+                System.out.println("#"); 
+            
+            } else {
+               
+                switch (i) {
+                    case 1:
+                        System.out.println("|      <><>      |");
+                        break;
+                    case 2:
+                        System.out.println("|    <>....<>    |");
+                        break;
+                    case 3:
+                        System.out.println("|  <>........<>  |");
+                        break;
+                    case 4:
+                        System.out.println("|<>............<>|");
+                        break;
+                    case 5:
+                        System.out.println("|<>............<>|");
+                        break;
+                    case 6:
+                        System.out.println("|  <>........<>  |");
+                        break;
+                    case 7:
+                        System.out.println("|    <>....<>    |");
+                        break;
+                    case 8:
+                        System.out.println("|      <><>      |");
+                        break;
+                }
+            }
+        }
+    }
+}
